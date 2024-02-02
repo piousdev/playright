86e408e6e5b057ec8aa04cff14cd77b83274df84
Pious Alpha <102190049+piousdev@users.noreply.github.com>
2024-02-02 18:17:49 +0100

    Update dependencies and add ClientForwardController

--- src/main/java/pious/playright/controller/ClientForwardController.java
+++ src/main/java/pious/playright/controller/ClientForwardController.java
@@ -1,2 +1,13 @@
-package pious.playright.controller;public class ClientForwardController {
+package pious.playright.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ClientForwardController {
+    @GetMapping(value = "/**/{path:[^.]*}")
+    public String forward(@PathVariable String path) {
+        return "forward:/";
+    }
 }
