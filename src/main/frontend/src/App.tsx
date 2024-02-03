72fc11db883df098028fb5fa443d67f4f700750b
Pious Alpha <102190049+piousdev@users.noreply.github.com>
2024-02-03 09:09:18 +0100

    Fix src/main/frontend tracking issue

---
+++ src/main/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button.tsx";
+import './App.css';
+
+function App() {
+
+  return (
+      <>
+          <p className="mb-2 font-bold underline text-9xl">
+              Hello world!
+          </p>
+          <Button size='lg'>Click Me</Button>
+      </>
+  )
+}
+
+export default App
