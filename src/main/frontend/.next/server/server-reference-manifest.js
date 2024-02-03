4775e5c1d0d2f79ed2a31e38bbbd691c8102e47c
Pious Alpha <102190049+piousdev@users.noreply.github.com>
2024-02-03 17:05:28 +0100

    Install vite

--- src/main/frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HTwcKJH3Kx8tAGBnlwCXfrYU0taJHgQDzDtFEvnv3rc=\"\n}"
--- src/main/frontend/.next/static/chunks/app/page-cf4dba012bda77b3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{8803:function(e,n,s){Promise.resolve().then(s.bind(s,4041)),Promise.resolve().then(s.bind(s,8999)),Promise.resolve().then(s.bind(s,747)),Promise.resolve().then(s.bind(s,1934)),Promise.resolve().then(s.t.bind(s,1749,23))}},function(e){e.O(0,[983,749,971,69,744],function(){return e(e.s=8803)}),_N_E=e.O()}]);
